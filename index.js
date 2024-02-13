c299e017a49e9bb3b187dde6b4c22fc2c06dafda
Shubham Tiwari <shubht3303@gmail.com>
2024-02-13 19:39:21 +0530

    added : express

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const fs = require('fs');
-
-function callback(err,data) {
-  console.log(data);
-
-}
-
-fs.readFile("a.txt", 'utf-8', callback);
-
 const express = require('express');
 const app = express();
+const port = 3000
+
+var calculateSum = function() {
+  var sum = 0;
+  for (let i = 0; i < 1000; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+function handleFirstRequest(req, res) {
+  var sum = 0;
+    for (let i = 0; i < 100; i++) {
+    sum = sum+i;
+  }
+  res.send('Sum is ' + sum);
+}
+
+app.get('/handlesum', handleFirstRequest)
+
+
+function startServer() {
+  console.log(`Example app listening on port ${port}`);
+}
+app.listen(port, startServer);
+
+//app.listen runs infinitely just like setInterval and exposes the express app to the required port.
